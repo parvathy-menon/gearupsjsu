af14c5827b3b0a605cb94996cabec0e9fc3b681e
Risako Numamoto <risako.numamoto@sjsu.edu>
2019-05-09 07:04:23 -0700

    Update DropClass.js

--- src/pages/DropClass.js
+++ src/pages/DropClass.js
@@ -1,51 +1,101 @@
 import React from 'react';
 import './Pages.css';
+//import classInfo from './classes';
+import Gymclass from './Gymclass';
 
 export default class DropClass extends React.Component {
+    state = {
+        gymclasses: [
+            {
+                id: '1',
+                name: 'Yoga',
+                instructor: 'Jill Smith',
+                days: 50,
+                time: 'MW 09:00 - 10:00',
+                contact: 'jill.smith@gearup.com'
+            },
+            {
+                id: '2',
+                name: 'Yoga',
+                instructor: 'Kevin Tracy',
+                days: 50,
+                time: 'TR 14:00 - 15:00',
+                contact: 'kevin.tracy@gearup.com'
+            },
+            {
+                id: '3',
+                name: 'Cycling',
+                instructor: 'Eve Jackson',
+                days: 60,
+                time: 'MW 13:00 - 14:00',
+                contact: 'eve.jackson@gearup.com'
+            },
+            {
+                id: '11',
+                name: 'Boxing',
+                instructor: 'Alan Odonnell',
+                days: 100,
+                time: 'MW 17:00 - 18:00',
+                contact: 'alan.odonnell@gearup.com'
+            },
+            {
+                id: '4',
+                name: 'Aerobics',
+                instructor: 'Carl Henson',
+                days: 30,
+                time: 'TR 10:00 - 11:00',
+                contact: 'carl.henson@gearup.com'
+            },
+            {
+                id: '5',
+                name: 'Pilates',
+                instructor: 'Janiah Bullock',
+                days: 30,
+                time: 'FR 11:00 - 13:00',
+                contact: 'janiah.bullock@gearup.com'
+            },
+            {
+                id: '6',
+                name: 'Weights',
+                instructor: 'Joe Macias',
+                days: 30,
+                time: '19:00 - 20:00',
+                contact: 'joe.macias@gearup.com'
+            }
+
+        ]
+    }
+    deleteClass = (index, e)=>{
+        const gymclasses = Object.assign([], this.state.gymclasses);
+        gymclasses.splice(index, 1);
+        this.setState({gymclasses:gymclasses});
+    }
+
     render() {
       return (
         <div className="App">
-            <div className="dropclass-grid">
-                <div className="heading">
-                    Drop Classes
-                </div>
-                <div className="table">
-                    <table>
-                        <tr className="table-heading">
-                            <th>Class</th>
-                            <th>Instructor</th> 
-                            <th>Days</th> 
-                            <th>Time</th>
-                            <th>Contact</th> 
-                            <th>Drop Class</th>
-                        </tr>
-                        <tr>
-                            <td>Jill</td>
-                            <td>Smith</td>
-                            <td>50</td>
-                            <td></td>
-                            <td></td>
-                            <td><button className="button">Drop Class</button></td>
-                        </tr>
-                        <tr>
-                            <td>Eve</td>
-                            <td>Jackson</td>
-                            <td>94</td>
-                            <td></td>
-                            <td></td>
-                            <td><button className="button">Drop Class</button></td>
-                        </tr>
-                        <tr>
-                            <td>John</td>
-                            <td>Doe</td>
-                            <td>80</td>
-                            <td></td>
-                            <td></td>
-                            <td><button className="button">Drop Class</button></td>
-                        </tr>
-                    </table>
-                </div>
+            <div className="heading">
+                Drop Classes
             </div>
+            <ul>
+                {
+                    this.state.gymclasses.map((gymclass, index)=>{
+                        return (
+                            <div>
+                                <Gymclass 
+                                name = {gymclass.name}
+                                instructor = {gymclass.instructor}
+                                days = {gymclass.days}
+                                time = {gymclass.time}
+                                contact = {gymclass.contact}
+                                delEvent={this.deleteClass.bind(this, index)}
+                                >{gymclass.id}</Gymclass>
+                            </div>
+                        );
+                    })
+                }
+            </ul>
+            <br />
         </div>
       );
     }
