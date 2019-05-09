fb364b6bd43c6b11691fb57b754ec244fb9867bd
Parvathy Menon <parvathy.menon1997@gmail.com>
2019-05-09 00:43:40 -0700

    Revert "facebook sdk"
    This reverts commit 45c63f105984e1e93b6c2bbfad1ee13956bd1cfe.

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -10,33 +10,6 @@ import {
 
 import LoginButton from "./LoginButton";
 
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: "451387118765518",
-    cookie: true,
-    xfbml: true,
-    version: "v3.3"
-  });
-
-  FB.AppEvents.logPageView();
-};
-
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
-
-(function(d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-
 const styles = {
   flex: {
     flex: 1
